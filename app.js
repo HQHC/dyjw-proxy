d14b00fb1efb4b9e991a54f9d6a707cda57eeb71
HQHC <996233002@qq.com>
2018-11-25 22:22:38 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,90 @@
-const express = require('express')
-const proxy = require('http-proxy-middleware')
+const Koa = require('koa')
+const Router = require('koa-router')
+const {fetch,post,getStream} = require('./frequest')
 
-const app = express();
+const {Base64Encode} = require('base64-stream')
+const app = new Koa();
+const router = Router();
+const koabody = require('koa-body')
+const cheerio = require('cheerio')
 
-const options = {
+router.get('/code',async ctx => {
+    let stream =  getStream('/verifycode.servlet')
+    const base64 =  await streamToBase64(stream)
+    ctx.response.body = base64
+})
 
-        target: 'http://jwgl.nepu.edu.cn', // target host
-        pathRewrite: (path,req) =>  path.replace('/dyjw',''),
-        onProxyRes(proxyRes, req, res) {
-            console.log(proxyRes['JSESSIONID'])
-        }
+router.post('/login',async ctx=>{
+    let {USERNAME,PASSWORD,RANDOMCODE} = ctx.request.body
+    let result = await login(USERNAME,PASSWORD,RANDOMCODE)
+    ctx.response.body = result
+})
+
+app.use(koabody())
+app.use(router.routes())
+
+app.listen(3000,() => {
+    console.log('object');
+})
+
+
+function streamToBase64(stream){
+    return new Promise(resolve => {
+        let data = 'data:image/jpeg;base64,'
+        let base64code = stream.pipe(new Base64Encode())
+        base64code.setEncoding('UTF8')
+        base64code.on('data',function(chunk){
+            data += chunk
+        })
+
+        base64code.on('end', function(){
+            resolve(data)     
+        })
+    })
 }
 
 
-app.use('/dyjw', proxy(options));
+async function login(name,pwd,code){
 
-app.listen(3001);
+    let main = await post('/Logon.do?method=logon',{
+        USERNAME:name,
+        PASSWORD:pwd,
+        RANDOMCODE:code
+    })
+
+    if (main.indexOf('http://jwgl.nepu.edu.cn/framework/main.jsp') == -1) {
+            return '用户名，密码或验证码错误'
+    }
+
+    let personalInfor = await fetch('/framework/main.jsp')
+
+    const $ = cheerio.load(personalInfor)
+
+    return $('title').text();
+
+    // return new Promise(resolve => {
+        
+    //     request({
+    //         url:'http://jwgl.nepu.edu.cn/Logon.do?method=logon',
+    //         method:'post',
+    //         form:{
+    //             USERNAME:name,
+    //             PASSWORD:pwd,
+    //             RANDOMCODE:code
+    //         }
+    //     },(err,response,body) => {
+    //         if (body.indexOf('http://jwgl.nepu.edu.cn/framework/main.jsp') == -1) {
+    //             resolve('用户名，密码或验证码错误')
+    //         }
+            
+    //         request('http://jwgl.nepu.edu.cn/framework/main.jsp',(err,response,body) => {
+                
+    //         })
+
+    //     })
+    // })
+}
+
+
+
+
